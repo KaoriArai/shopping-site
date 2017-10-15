0d2a7bee3b408046834e0b412fe2ce8c1580394b
NappingCat <nappingcat0711@gmail.com>
2017-10-15 09:19:19 +0000

    1st commit

---
+++ carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#cover_area').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots:true,
+            items: 1,
+            autoplay: false
+        }
+    );
+});
